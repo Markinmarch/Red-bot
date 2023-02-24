42e702b630132ec5b311ab3fd15218bd859ef310
Mark Izotov <iztvmrk@mail.ru>
2023-02-24 14:38:52 +0300

    DELETE_ALL

--- street_assistant_bot/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import start
-from . import cancel
-from . import process_name
-from . import age_filter
-from . import process_age
-from . import process_gender
--- street_assistant_bot/states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class Form(StatesGroup):
-    name = State()
-    age = State()
-    gender = State()
