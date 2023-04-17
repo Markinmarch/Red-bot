98b03520a50c29cc1df8aeebcd2d513bae6cf6e1
iztvmark <iztvmrk@mail.ru>
2023-04-17 23:04:59 +0300

    удаление папки markups

--- red_bot/app/advertisement_handlers/choise_advertisement.py
+++ red_bot/app/advertisement_handlers/choise_advertisement.py
@@ -6,16 +6,18 @@ from red_bot.settings.setting import dp
 
 @dp.callback_query_handler(text = 'authorization')
 async def authorization_old_user(callback: types.CallbackQuery):
-    markup = types.InlineKeyboardMarkup(resize_keyboard = True, selective = True)
+    inline_markup = types.InlineKeyboardMarkup()
     jobs_btn = types.InlineKeyboardButton(text = 'Работа', callback_data = 'jobs')
     services_btn = types.InlineKeyboardButton(text = 'Услуги', callback_data = 'services')
     offers_btn = types.InlineKeyboardButton(text = 'Предложения', callback_data = 'offers')
     exchanges_btn = types.InlineKeyboardButton(text = 'Биржа', callback_data = 'exchanges')
-    markup.add(
+    inline_markup.add(
         jobs_btn,
         services_btn,
         offers_btn,
         exchanges_btn
     )
     await callback.answer(text = 'Подождите, проходит авторизация.')
-    await callback.message.answer(text = 'Выберите направление', reply_markup = markup)
+    await callback.message.answer(
+        text = 'Выберите направление',
+        reply_markup = inline_markup)
--- red_bot/app/registration_handlers/beggin.py
+++ red_bot/app/registration_handlers/beggin.py
@@ -3,36 +3,28 @@ from aiogram import types
 
 from red_bot.settings.setting import dp
 from red_bot.sql_db import db
+from red_bot.app.content import text_message
 
 
 @dp.callback_query_handler(text = 'create_account')
 async def user_agreement_via_query(callback: types.CallbackQuery):
-    markup = types.InlineKeyboardMarkup(resize_keyboard = True)
+    inline_markup = types.InlineKeyboardMarkup(resize_keyboard = True)
     agree_btn = types.InlineKeyboardButton(text = '📝 Продолжить', callback_data = 'agree')
-    markup.add(agree_btn)
     await callback.message.answer(
-        text = 'Привет! Я бот, который поможет Вам найти быструю работу или разместить своё объявление с предложением.\n'
-        'Но для начала Вам необходимо ознакомиться с некоторыми правилами и юридическими аспектами, чтобы не стать жертвой мошенников.\n'
-        'Для этого перейди по <b><a href = "https://telegra.ph/Pravila-polzovaniya-telegram-botom-02-25">ссылке</a></b> и прочти обязательно правила!\n'
-        'Ваша дальнейшая регистрация автоматически подтверждает, что Вы ознакомились с правилами.',
+        text = text_message.WELCOME,
         parse_mode = 'HTML',
-        reply_markup = markup
+        reply_markup = inline_markup.add(agree_btn)
     )
-
 
 @dp.message_handler(commands=['create_account'])
 async def user_agreement_via_command(message: types.Message):
-    markup = types.InlineKeyboardMarkup(resize_keyboard = True)
+    inline_markup = types.InlineKeyboardMarkup(resize_keyboard = True)
     agree_btn = types.InlineKeyboardButton(text = '📝 Продолжить', callback_data = 'agree')
-    markup.add(agree_btn)
     if message.from_user.id in db.database.ids_users():
         await message.answer(text = '<b>У Вас уже имеется учётная запись</b>', parse_mode = 'HTML')
     else:
         await message.answer(
-            text = 'Привет! Я бот, который поможет Вам найти быструю работу или разместить своё объявление с предложением.\n'
-            'Но для начала Вам необходимо ознакомиться с некоторыми правилами и юридическими аспектами, чтобы не стать жертвой мошенников.\n'
-            'Для этого перейди по <b><a href = "https://telegra.ph/Pravila-polzovaniya-telegram-botom-02-25">ссылке</a></b> и прочти обязательно правила!\n'
-            'Ваша дальнейшая регистрация автоматически подтверждает, что Вы ознакомились с правилами.',
+            text = text_message.WELCOME,
             parse_mode = 'HTML',
-            reply_markup = markup
+            reply_markup = inline_markup.add(agree_btn)
         )
