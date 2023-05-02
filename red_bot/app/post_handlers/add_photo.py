60041a4e6e6496caea07157b27eb080150614c78
iztvmark <iztvmrk@mail.ru>
2023-05-02 03:18:35 +0300

    добавление пользователем одной фотографии

--- red_bot/app/post_handlers/add_photo.py
+++ red_bot/app/post_handlers/add_photo.py
@@ -5,49 +5,15 @@ from aiogram.dispatcher import FSMContext
 from red_bot.settings.setting import dp
 from red_bot.utils.state import AddRecord
 from red_bot.utils.keyboards.inline_keyboard import publish_button
-from red_bot.utils.keyboards.replay_keyboard import continue_add_photo_button
 
 
-@dp.message_handler(state = AddRecord.first_photo, content_types = types.ContentType.PHOTO)
+@dp.message_handler(state = AddRecord.photo, content_types = types.ContentType.PHOTO)
 async def add_first_photo__cmd_second_photo(message: types.Message, state: FSMContext):
     # записываем имя пользователя
     async with state.proxy() as user_data:
-        user_data['first_photo'] = message.photo[0].file_id
+        user_data['photo'] = message.photo[0].file_id
     await AddRecord.next()
     await message.answer(
-        text = 'Можете добавить фотографию, либо начать публикацию. Для этого нажмите кнопку "Продолжить"',
-        reply_markup = continue_add_photo_button
+        text = 'Теперь Вы можете опубликовать свою запись',
+        reply_markup = publish_button
     )
-
-@dp.message_handler(state = AddRecord.second_photo, content_types = types.ContentType.PHOTO)
-async def add_second_photo__cmd_third_photo(message: types.Message, state: FSMContext):
-    # записываем имя пользователя
-    if message.text == 'Подолжить':
-        message.answer(
-            text = None,
-            reply_markup = publish_button
-        )
-    else:
-        async with state.proxy() as user_data:
-            user_data['second_photo'] = message.photo[0].file_id
-        await AddRecord.next()
-        await message.answer(
-            text = 'Можете добавить фотографию, либо начать публикацию. Для этого нажмите кнопку "Продолжить"',
-            reply_markup = continue_add_photo_button
-        )
-
-@dp.message_handler(state = AddRecord.third_photo, content_types = types.ContentType.PHOTO)
-async def add_third_photo__cmd_publish(message: types.Message, state: FSMContext):
-    # записываем имя пользователя
-    if message.text == 'Подолжить':
-        message.answer(
-            text = None,
-            reply_markup = publish_button
-        )
-    else:
-        async with state.proxy() as user_data:
-            user_data['third_photo'] = message.photo[0].file_id
-        await message.answer(
-            text = 'Готово! Теперь Вы можете опубликовать свой пост на канале',
-            reply_markup = publish_button
-        )
--- red_bot/app/post_handlers/publication_post.py
+++ red_bot/app/post_handlers/publication_post.py
@@ -3,7 +3,6 @@ from aiogram.dispatcher import FSMContext
 
 
 from red_bot.settings.setting import dp
-from red_bot.settings.setting import bot
 from red_bot.utils.state import AddRecord
 from red_bot.settings.config import CHANNEL_ID
 from red_bot.utils.keyboards.inline_keyboard import under_post_buttons
@@ -12,25 +11,16 @@ from red_bot.utils.keyboards.inline_keyboard import under_post_buttons
 @dp.callback_query_handler(text = 'publish', state = AddRecord)
 async def user_publish_post(callback: types.CallbackQuery, state: FSMContext):
     post_data = await state.get_data()
-    media = types.MediaGroup()
-    media.attach_photo(types.InputMediaPhoto(
-        post_data.get('first_photo'),
+    await callback.bot.send_photo(
+        chat_id = CHANNEL_ID,
+        photo = post_data.get('photo'),
         caption = (
             f'<b>{post_data.get("direction")}: <u>{post_data.get("title")}</u></b>\n'
             f'→ {post_data.get("text")}\n'
             f'→ <i>{post_data.get("conditions")}</i>'
-        )
-    ))
-    media.attach_photo(types.InputMediaPhoto(post_data.get('second_photo')))
-    media.attach_photo(types.InputMediaPhoto(post_data.get('third_photo')))
-    await callback.bot.send_media_group(
-        chat_id = CHANNEL_ID,
-        media = media
-        # reply_markup = under_post_buttons
+        ),
+        parse_mode = 'HTML',
+        reply_markup = under_post_buttons
     )
-    # await callback.bot.send_message(
-    #     chat_id = CHANNEL_ID,
-    #     text = 'callback.from_user.first_name',
-    #     reply_markup = under_post_buttons,
-    # )
+
     await state.finish()
