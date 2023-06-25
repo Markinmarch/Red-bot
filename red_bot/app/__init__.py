3d59c8c4997404105fb3d599e7b110293c75abf1
mrkinmrch <iztvmrk@mail.ru>
2023-06-26 01:10:33 +0300

    удаление выбора дирекций

--- red_bot/app/__init__.py
+++ red_bot/app/__init__.py
@@ -1,4 +1,3 @@
 from . import account_handlers
-from . import directions_handlers
 from . import post_handlers
 from . import connecting_handlers
--- red_bot/app/directions_handlers/jobs_directory.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-
-
-from red_bot.settings.setting import dp
-
-
-@dp.callback_query_handler(text = 'jobs')
-async def jobs_directory(callback: types.CallbackQuery):
-    pass
