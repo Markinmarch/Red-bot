a4c902e7de3e014384ddc0ab3ceb37dd6e6c6790
mrkinmrch <iztvmrk@mail.ru>
2023-07-02 04:24:37 +0300

    добавлен фильтр по вермени + фильтр к зареганым

--- red_bot/app/post_handlers/post_instruction.py
+++ red_bot/app/post_handlers/post_instruction.py
@@ -1,15 +1,31 @@
 from aiogram import types
+from aiogram.utils.exceptions import Throttled
 
 
 from red_bot.settings.setting import dp
-from red_bot.utils.content.text_content import POST_INSTRUCTION
-from red_bot.utils.keyboards.inline_keyboard import continue_filling_button
+from red_bot.sql_db import users_db
+from red_bot.utils.content.text_content import POST_INSTRUCTION, WAITING_MESSAGE, UNREGISTRED_USER
+from red_bot.utils.keyboards.inline_keyboard import continue_filling_button, start_registration_button
 
 
 @dp.message_handler(commands=['create_post'])
 async def user_rules_reminder(message: types.Message):
-    await message.answer(
-        text = POST_INSTRUCTION,
-        parse_mode = 'HTML',
-        reply_markup = continue_filling_button
-    )
+    try:
+        if message.from_user.id not in users_db.users_database.ids_users():
+            pass
+        await dp.throttle('create_post', rate = 300)
+    except Throttled:
+        await message.answer(
+            text = WAITING_MESSAGE
+        )
+    except TypeError:
+        await message.answer(
+            text = UNREGISTRED_USER.format(message.from_user.first_name),
+            reply_markup = start_registration_button
+        )
+    else:
+        await message.answer(
+            text = POST_INSTRUCTION,
+            parse_mode = 'HTML',
+            reply_markup = continue_filling_button
+        )
