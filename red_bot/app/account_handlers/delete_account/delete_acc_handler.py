6d5afe1f0ecc9a9444e221b7f7369eb58aa9710b
mrkinmrch <iztvmrk@mail.ru>
2023-07-02 04:25:11 +0300

    унификация

--- red_bot/app/account_handlers/delete_account/delete_acc_handler.py
+++ red_bot/app/account_handlers/delete_account/delete_acc_handler.py
@@ -5,7 +5,8 @@ import logging
 from red_bot.settings.setting import dp
 from red_bot.utils.keyboards.inline_keyboard import delete_acc_button
 from red_bot.utils.commands import set_commands_for_new_user
-from red_bot.sql_db import db
+from red_bot.sql_db import users_db
+from red_bot.utils.content.text_content import DELETE_ACCOUNT_MESSAGE
 
 
 @dp.message_handler(commands = ['delete_account'])
@@ -20,20 +21,20 @@ async def delete_account(message: types.Message):
 async def erase_user_data(callback: types.CallbackQuery):
     await set_commands_for_new_user(bot = callback.bot)
     try:
-        if callback.from_user.id in db.users_database.ids_users():
-            db.users_database.delete_users(user_id = callback.from_user.id)
+        if callback.from_user.id in users_db.users_database.ids_users():
+            users_db.users_database.delete_users(user_id = callback.from_user.id)
             await callback.answer(
-                text = 'Ваш аккаунт удалён',
+                text = DELETE_ACCOUNT_MESSAGE,
                 show_alert = True
             )
             logging.info(f'User {callback.from_user.id} has been deleted')
         else:
             await callback.answer(
-                text = 'Ваш аккаунт уже удалён',
+                text = DELETE_ACCOUNT_MESSAGE,
                 show_alert = True
             )
     except TypeError:
         await callback.answer(
-            text = 'Ваш аккаунт уже удалён',
+            text = DELETE_ACCOUNT_MESSAGE,
             show_alert = True
         )
