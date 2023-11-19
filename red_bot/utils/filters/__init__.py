68bc451be603d9af7ef4ef495c8f5b2369d5727e
iztvmrk <iztvmrk@mail.ru>
2023-11-19 23:47:17 +0300

    entered it back filters

--- red_bot/utils/filters/__init__.py
+++ red_bot/utils/filters/__init__.py
@@ -1,4 +1,3 @@
-from . import filters_by_registration_user
 from . import filters_by_created_posts
 from . import filters_by_bad_words
 from . import brake_states
--- red_bot/utils/filters/filters_by_created_posts.py
+++ red_bot/utils/filters/filters_by_created_posts.py
@@ -1,29 +1,28 @@
-from aiogram import types
+from aiogram import types, F
 
 
 from red_bot.settings.setting import dp
-from red_bot.utils.keyboards.reply_keyboard import direction_detection_buttons
 from red_bot.utils.state import AddPost
 from red_bot.utils.content.text_content import FILTERS_MESSAGE
 
     
-@dp.message_handler(
-    lambda message: len(message.text) > 20,
+@dp.message(
+    len(F.text) > 20,
     state = AddPost.title
 )
 async def repeat_enter_title(message: types.Message):
     await message.answer(text = FILTERS_MESSAGE['create_post']['repeat_title'])
 
-@dp.message_handler(
-    lambda message: len(message.text) < 20,
+@dp.message(
+    len(F.text) < 20,
     state = AddPost.text
 )
 async def repeat_enter_text(message: types.Message):
     await message.answer(text = FILTERS_MESSAGE['create_post']['repeat_text'])
 
-@dp.message_handler(
-    lambda message: message.text not in [button[0]['text'] for button in direction_detection_buttons['keyboard']],
-    state = AddPost.direction
-)
-async def repeat_enter_direction(message: types.Message):
-    await message.answer(text = FILTERS_MESSAGE['create_post']['repeat_direction'])
+# @dp.message(
+#     lambda message: message.text not in [button[0]['text'] for button in direction_detection_buttons['keyboard']],
+#     state = AddPost.direction
+# )
+# async def repeat_enter_direction(message: types.Message):
+#     await message.answer(text = FILTERS_MESSAGE['create_post']['repeat_direction'])
