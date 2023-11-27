20174f7b925cc803c4aa68e01bf54618d79f8f77
Markinmarch <iztvmrk@mail.ru>
2023-11-27 11:25:55 +0300

    Revert "Update __init__.py"
    This reverts commit 9354d067f54b2b3f29205b4459655b6074fa1fbc.

--- red_bot/sql_db/__init__.py
+++ red_bot/sql_db/__init__.py
@@ -1,4 +1,38 @@
+import logging
+import os
+import sqlite3
+
+
 from . import bot_tables
 from . import posts_db
 from . import responders_db
 from . import users_db
+
+
+from ..settings.config import DATA_PATH, DB_NAME
+
+
+
+DB_tables = bot_tables.Bot_tables_DB(
+    db_name = DB_NAME,
+    db_path = DATA_PATH
+)
+
+def create_db() -> None:
+    if DB_NAME + '.db' not in os.listdir(DATA_PATH):
+        DB_tables.create_users_table()
+        DB_tables.create_posts_table()
+        DB_tables.create_responders_table()
+    else:
+        logging.info('--- Database for "SEVASTOPOL ADJUTOR BOT" connection established ---')
+
+def start_db() -> None:
+    try:
+        create_db()
+    except sqlite3.OperationalError:
+        os.mkdir('red_bot/datas')
+        create_db()
+
+posts = posts_db.Posts()
+users = users_db.Users()
+responders = responders_db.Responders()
