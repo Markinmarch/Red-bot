5bdad8dc04862153554bced0504cbaf89eb23e7a
Markinmarch <iztvmrk@mail.ru>
2023-12-05 21:24:39 +0300

    upd filters - add use brake buttons

--- red_bot/utils/filters/brake_states.py
+++ red_bot/utils/filters/brake_states.py
@@ -4,41 +4,10 @@ from aiogram.fsm.context import FSMContext
 
 from red_bot.settings.setting import dp
 from red_bot.utils.content.text_content import FILTERS_MESSAGE
-from red_bot.utils.state import AddPost, AddUser, DeletePost
 
-@dp.message(
-    AddUser,
-    F.text == 'Отменить ❌'
-)
-async def brake_state_AddUser(
-    message: types.Message,
-    state: FSMContext
-):
-    await message.answer(
-        text = FILTERS_MESSAGE['command_brake'],
-        reply_markup = types.ReplyKeyboardRemove()
-        )
-    await state.clear()
 
-@dp.message(
-    AddUser,
-    F.text == 'Отменить ❌'
-)
-async def brake_state_AddPost(
-    message: types.Message,
-    state: FSMContext
-):
-    await message.answer(
-        text = FILTERS_MESSAGE['command_brake'],
-        reply_markup = types.ReplyKeyboardRemove()
-        )
-    await state.clear()
-
-@dp.message(
-    AddUser,
-    F.text == 'Отменить ❌'
-)
-async def brake_state_DeletePost(
+@dp.message(F.text == 'Отменить ❌')
+async def brake(
     message: types.Message,
     state: FSMContext
 ):
