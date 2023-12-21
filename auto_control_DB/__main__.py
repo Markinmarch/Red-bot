a1f041331037584dc438e230313078bb8f50fc11
Markinmarch <iztvmrk@mail.ru>
2023-12-21 22:29:23 +0300

    fix bug control DB

--- auto_control_DB/__main__.py
+++ auto_control_DB/__main__.py
@@ -3,7 +3,7 @@ import time
 if __name__ == '__main__':
     from auto_control_DB import check_DB
     while True:
-        from check_DB import *
+        from .check_DB import *
         if time.strftime('%H') == DROP_TIME:
             write_datas
             erase_db
